6f3337483b11cb03da653f917f23e70712c6285c
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-04 20:20:24 -0300

    Installed RSpec and write initial tests for user and yell models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it "creates a valid user" do
+    user1 = User.create(username: "arthurborges", full_name: "Arthur Borges")
+    expect(user1.valid?).to eq(true)
+  end
+
+  it "not allows the creation of a user without username" do
+    user1 = User.create(full_name: "Arthur Borges")
+    expect(user1.valid?).to eq(false)
+  end
+end
---
+++ spec/models/yell_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Yell, type: :model do
+  it "accepts a valid message" do
+    user1 = User.create(username: "arthurborges", full_name: "Arthur Borges")
+    yell1 = user1.yells.create(text: "HELP! I'M OVERWHELMED!!")
+    expect(yell1.valid?).to eq(true)
+  end
+
+  it "does not accept a message from without an user" do
+    yell1 = Yell.create(text: "HELP! I'M OVERWHELMED!!")
+    expect(yell1.valid?).to eq(false)
+  end
+
+  it "does not accept a empty message" do
+    yell1 = Yell.create
+    expect(yell1.valid?).to eq(false)
+  end
+end

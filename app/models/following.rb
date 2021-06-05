d94a26787a3192300dd5713a6c9838a643c44226
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-05 11:01:12 -0300

    Removed validation from Following model, as it only contains foreign keys

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,6 +1,4 @@
 class Following < ApplicationRecord
-  validates :followers, :followeds, presence: true
-
   belongs_to :author, class_name: 'User', foreign_key: 'followed_id'
   belongs_to :author, class_name: 'User', foreign_key: 'follower_id'
 end

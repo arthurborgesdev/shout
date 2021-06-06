55f3e061f43cf76bd84b8727154012ef14736e5b
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-06 15:37:14 -0300

    SApply styling a little bit more and start creating the partials with forms and list for yell model

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,5 +3,6 @@ class HomepageController < ApplicationController
     @user = User.find(session[:user_id])
     @followers = @user.followers.count
     @followeds = @user.followeds.count
+    @yell = Yell.new
   end
 end

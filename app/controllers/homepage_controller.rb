a9b7d9744f204d0c046d522006e1ca86a555b358
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-05 22:37:16 -0300

    Add more styling to the picture, sub-texts and menu items and displayed number of followeds and followers

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,7 @@
 class HomepageController < ApplicationController
   def index
     @user = User.find(session[:user_id])
+    @followers = @user.followers.count
+    @followeds = @user.followeds.count
   end
 end

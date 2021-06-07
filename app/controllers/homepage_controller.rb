f40e1292dbca458562f5c2fb41c1437417e8caaf
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-07 11:54:49 -0300

    Start to customize, convert into partials and stylize the signup and login pages

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,6 @@
 class HomepageController < ApplicationController
+  before_action :require_login, only: %i[ index show edit update destroy ]
+  
   def index
     @user = User.find(session[:user_id])
     @followers_count = @user.followers.count

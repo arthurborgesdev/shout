0d0a30a5c717513b84ef3a28eb6bb219a9738485
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-07 16:39:21 -0300

    Working on userpage and homepage inner pages - partials

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,8 +3,6 @@ class HomepageController < ApplicationController
   
   def index
     @user = User.find(session[:user_id])
-    @followers_count = @user.followers.count
-    @followeds_count = @user.followeds.count
     @yell = Yell.new
     @yells = Yell.all
   end

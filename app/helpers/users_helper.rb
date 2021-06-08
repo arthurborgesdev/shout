1be352efdcd38f19f8a7839dce2413b1b436ed58
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-08 19:35:13 -0300

    List correctly the who to follow people of a user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def who_to_follow_list(follower)
+    res = ''
+
+    followers_list = current_user.followers.pluck(:followed_id)
+
+    unless followers_list.include?(follower.id) || current_user.id == follower.id
+      res << "<div class='d-flex'>"
+      res <<   "<p class='follow-picture'>#{follower.photo}</p>"
+      res <<   "<div class='d-flex flex-column my-auto'>"
+      res <<     "<p>#{follower.full_name}</p>"
+      res <<   "</div>"
+      res << "</div>"
+    end
+    res.html_safe 
+  end
 end

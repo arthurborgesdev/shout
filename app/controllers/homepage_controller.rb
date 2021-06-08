c180cd5b66102cface88803e458a3747ac0b5518
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-08 20:30:37 -0300

    Finish the list of who to follow and start to implement followed by

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,7 @@ class HomepageController < ApplicationController
   before_action :require_login, only: %i[ index show edit update destroy ]
   
   def index
-    @users = User.all
+    @users = User.all.order(created_at: :desc)
     @user = User.find(session[:user_id])
     @yell = Yell.new
     @yells = Yell.all.order(created_at: :desc)
--- app/models/following.rb
+++ app/models/following.rb
@@ -1,6 +1,4 @@
 class Following < ApplicationRecord
   belongs_to :followed, class_name: 'User', foreign_key: 'followed_id'
   belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
-
-  scope :not_followed_by, ->(user) { where.not(followed_id: user) }
 end

f685a42b2d3f3529a933fc26468479de7797ad97
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-09 18:19:20 -0300

    Finish adding model user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe User, type: :model do
     user1 = User.create(full_name: "Arthur Borges")
     expect(user1.valid?).to eq(false)
   end
+
+  it "not allows the creation of a user without Full Name" do
+    user1 = User.create(username: "arthurborges")
+    expect(user1.valid?).to eq(false)
+  end
 end

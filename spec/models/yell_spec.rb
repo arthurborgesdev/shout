17989d470b062fdb78db07076adb8d4b26b54444
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-09 18:56:51 -0300

    Add database-cleaner and finish adding all tests to Yell model

--- spec/models/yell_spec.rb
+++ spec/models/yell_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe Yell, type: :model do
     yell1 = Yell.create
     expect(yell1.valid?).to eq(false)
   end
+
+  it "destroys a yell successfully" do
+    DatabaseCleaner.clean
+    user1 = User.create(username: "arthurborges", full_name: "Arthur Borges")
+    yell1 = user1.yells.create(text: "I'M GETTING OUTTA HERE!")
+
+    expect { yell1.destroy }.to change { Yell.count }.by(-1)
+  end
 end

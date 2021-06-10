12d4f0079e3ab34fcf3d7f6045b8df2ccbc09895
Arthur Borges <arthurborges.dev@gmail.com>
2021-06-09 21:22:03 -0300

    Remove migration of removing foreign key

--- db/migrate/20210605033824_remove_foreign_key_from_followings.rb
+++
@@ -1,6 +0,0 @@
-class RemoveForeignKeyFromFollowings < ActiveRecord::Migration[6.1]
-  def change
-    remove_foreign_key :followings, column: :followed_id
-    remove_foreign_key :followings, column: :follower_id
-  end
-end
